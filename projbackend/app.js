44b46b4e11897db5238d42d4510471eedd306b7f
Mayank Jain <jainmayank16041999@gmail.com>
2020-06-30 18:07:07 +0530

    app.js updated

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,2 +1,47 @@
-//Sarvesh is fat
-//Confilit!
+//Imports
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors")
+
+//My Routes
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+const postRoutes = require("./routes/post")
+const notificationRoutes = require("./routes/notification")
+
+
+//DB Connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+});
+
+
+//Middlewares
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+//Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", postRoutes);
+app.use("/api", notificationRoutes);
+
+
+//Port
+const port = process.env.PORT || 8000
+
+
+//Server Start
+app.listen(port, () => {
+    console.log(`App is running at ${port}`);
+})

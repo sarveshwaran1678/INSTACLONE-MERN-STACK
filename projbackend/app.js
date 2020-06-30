ca0e68f0a1934a3e97d72e7f3d414e85b4c00875
Mayank Jain <jainmayank16041999@gmail.com>
2020-06-30 18:34:06 +0530

    cors removed

--- projbackend/app.js
+++ projbackend/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+
 
 //My Routes
 const authRoutes = require('./routes/auth');
@@ -27,7 +27,7 @@ mongoose
 //Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+
 
 //Routes
 app.use('/api', authRoutes);

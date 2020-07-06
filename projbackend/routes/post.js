fb7044f1fca38a48eb7c4c8f35d01be782cb03ff
Mayank Jain <jainmayank16041999@gmail.com>
2020-07-06 14:37:10 +0530

    routes post managed

--- projbackend/routes/post.js
+++ projbackend/routes/post.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { getPictureById, getAnotherPictureById } = require("../controllers/post")
+const { getPictureById, getAnotherPictureById, createComment } = require("../controllers/post")
 const { isSignedIn, isAuthenticated } = require('../controllers/auth');
+const { getUserById } = require("../controllers/user")
 
 //parameter extractor
 router.param('userId', getUserById);
 router.param("picId", getPictureById)
 router.param("anotherPicId", getAnotherPictureById)
 
-router.post("/comment/newComment/:userId/:picId", isSignedIn, isAuthenticated,)
+
+//res.body must pass commentBody with it
+router.post("/comment/newComment/:userId/:picId", isSignedIn, isAuthenticated, createComment)
 
 
 

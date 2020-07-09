91aa07a68452962d4592b1e586130a11180d7b43
Mayank Jain <jainmayank16041999@gmail.com>
2020-07-09 10:52:34 +0530

    getNotifications & searchUsers made , testing left

---
+++ projbackend/controllers/navbar.js
@@ -0,0 +1,33 @@
+const User = require('../models/user');
+
+
+
+exports.getNotifications = (req, res) => {
+    let user = req.profile
+
+    let skipCount = req.body.skipCount || 0
+
+    User.findById(user._id)
+        .populate("UpdateNotification.UserId", "username profilePicPath")
+        .limit(8)
+        .skip(8 * skipCount)
+        .then((notification) => {
+            return res.send(notification)
+        }).catch((err) => {
+            return res.status(500).send(err)
+        })
+}
+
+exports.searchUsers = (req, res) => {
+    let skipCount = req.body.skipCount || 0
+
+    User.find({ username: req.body.searchTerm })
+        .limit(10)
+        .skip(10 * skipCount)
+        .then((result) => {
+            return res.send(result)
+        })
+        .catch((err) => {
+            return res.status(500).send(err)
+        })
+}
--- projbackend/routes/navbar.js
+++ projbackend/routes/navbar.js
@@ -1,1 +1,27 @@
 //search by api get call
+
+const express = require('express');
+const router = express.Router();
+
+const { isSignedIn, isAuthenticated } = require('../controllers/auth');
+const { getUserById } = require('../controllers/user');
+const { getNotifications, searchUsers } = require('../controllers/navbar');
+
+router.param('userId', getUserById);
+
+//to get all you notification
+//pass a skip count in req.body
+router.get("/getNotification/:userId", isSignedIn, isAuthenticated, getNotifications)
+
+//to get all your followings stories
+
+
+//to search
+//will send each stroke onChange in req.body.searchTerm
+//req.body.skipCount will contain no. of times user has clicked load more
+router.get("/searchUsers/:userId", isSignedIn, isAuthenticated, searchUsers)
+
+//user feed
+
+
+module.exports = router;

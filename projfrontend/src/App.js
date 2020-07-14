10993ea99ebce6bfd9f90855f9b269e201267ab2
RajenderSinghB <prince00430@gmail.com>
2020-07-14 12:15:55 +0530

    frontend started

--- projfrontend/src/App.js
+++ projfrontend/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage, FormikProps } from 'formik';
 import * as Yup from 'yup';
+import './style.css';
 
+import insta from './Images/insta.png';
 const initialValues = {
     email: '',
     password: '',
@@ -27,48 +29,94 @@ const validationSchema = Yup.object().shape({
 function App() {
     return (
         <div class='container'>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={onSubmit}>
-                {({ errors, touched }) => (
-                    <Form>
-                        <div class='input-group mb-3'>
-                            <Field
-                                className='form-control'
-                                type='email'
-                                name='email'
-                                placeholder='Enter Your Email'
-                                style={{ outline: 'none', shadow: 'none' }}
-                            />
-                            {errors.email && touched.email ? (
-                                <div class='input-group-append'>
-                                    <span class='input-group-text'>
-                                        <i class='fas fa-times'></i>
-                                    </span>
-                                </div>
-                            ) : (
-                                <div class='input-group-append'>
-                                    <span class='input-group-text'>
-                                        <i class='far fa-times-circle'></i>
-                                    </span>
-                                </div>
-                            )}
-                        </div>
-                        <div className='form-group'>
-                            <Field
-                                className='form-control'
-                                type='text'
-                                name='password'
-                                placeholder='Enter Your Password'
-                            />
-                            <ErrorMessage name='password' />
-                        </div>
+            <div class='row'>
+                <div class='col-md-6'>
+                    <img src={insta} />
+                </div>
+                <div
+                    class='col-md-4'
+                    style={{ border: '1px solid black', borderRadius: '10px' }}>
+                    <h2 style={{ marginTop: '10vh' }}>Instagram</h2>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}>
+                        {({ errors, touched, values }) => (
+                            <Form>
+                                <div class='input-group mb-3 '>
+                                    <div class='input-group-prepend'>
+                                        <span class='input-group-text pre-text'>
+                                            <i class='fas fa-envelope'></i>
+                                        </span>
+                                    </div>
+                                    <Field
+                                        className='form-control'
+                                        type='email'
+                                        name='email'
+                                        placeholder='Enter Your Email'
+                                        style={{
+                                            backgroundcolor: 'white',
+                                        }}
+                                    />
 
-                        <button type='submit'>Submit</button>
-                    </Form>
-                )}
-            </Formik>
+                                    {values.email.length === 0 ? (
+                                        <div class='input-group-append '>
+                                            <span class='input-group-text post-text'></span>
+                                        </div>
+                                    ) : values.email.length > 0 &&
+                                      errors.email ? (
+                                        <div class='input-group-append p-0 '>
+                                            <span class='input-group-text'>
+                                                <i class='fas fa-times'></i>
+                                            </span>
+                                        </div>
+                                    ) : (
+                                        <div class='input-group-append '>
+                                            <span class='input-group-text'>
+                                                <i class='fas fa-check'></i>
+                                            </span>
+                                        </div>
+                                    )}
+                                </div>
+                                <div class='input-group mb-3'>
+                                    <div class='input-group-prepend'>
+                                        <span class='input-group-text'>
+                                            <i class='fas fa-user-lock'></i>
+                                        </span>
+                                    </div>
+                                    <Field
+                                        className='form-control'
+                                        type='text'
+                                        name='password'
+                                        placeholder='Enter Your Password'
+                                    />
+                                    {values.password.length === 0 ? (
+                                        <div class='input-group-append'>
+                                            <span class='input-group-text'></span>
+                                        </div>
+                                    ) : values.password.length > 0 &&
+                                      errors.password ? (
+                                        <div class='input-group-append'>
+                                            <span class='input-group-text'>
+                                                <i class='fas fa-times'></i>
+                                            </span>
+                                        </div>
+                                    ) : (
+                                        <div class='input-group-append'>
+                                            <span class='input-group-text'>
+                                                <i class='fas fa-check'></i>
+                                            </span>
+                                        </div>
+                                    )}
+                                </div>
+
+                                <button type='submit'>Submit</button>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+                <div class='col-md-2'></div>
+            </div>
         </div>
     );
 }

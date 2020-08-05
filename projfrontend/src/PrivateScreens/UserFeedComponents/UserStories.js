e76fea3d45ee9c6f057ff6360659d6eaa7ff5a8d
Mayank Jain <jainmayank16041999@gmail.com>
2020-08-05 19:31:20 +0530

    toast

--- projfrontend/src/PrivateScreens/UserFeedComponents/UserStories.js
+++ projfrontend/src/PrivateScreens/UserFeedComponents/UserStories.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Image, Transformation, Placeholder } from "cloudinary-react";
+import { ToastContainer, toast, style } from "react-toastify";
 
 import StoryModal from "./StoryModal";
 import { getAnotherUserDetails } from "./APICalls";
+
+import post from "../../Images/mayankPost.jpg";
 
 const CloudName = process.env.REACT_APP_CLOUDNAME;
 
 function UserStories({ story }) {
   const [userName, setUserName] = useState("");
   const [timeBefore, setTimeBefore] = useState("");
-
-  // console.log("====================================");
-  // console.log(diff);
-  // console.log("====================================");
 
   useEffect(() => {
     getDetails();
@@ -43,35 +42,62 @@ function UserStories({ story }) {
       });
   };
 
+  const ShowModal = () => (
+    <div class="d-none d-md-block w-100">
+      <Image
+        className="m-1"
+        cloudName={CloudName}
+        loading="lazy"
+        publicId={story.picturePath}
+      >
+        <Transformation crop="scale" gravity="center" />
+        <Placeholder type="pixelate" />
+      </Image>
+    </div>
+  );
+
+  const notify = () => {
+    toast(<ShowModal />);
+  };
+
   return (
-    <div
-      className="row ml-0 mt-2 "
-      style={{ overflowY: "visible", fontWeight: "500" }}
-      onClick={() => {
-        return <StoryModal />;
-      }}
-    >
-      <div className="col-3 text-right" style={{ paddingRight: "0" }}>
-        <Image
-          className="m-1"
-          cloudName={CloudName}
-          loading="lazy"
-          publicId={story.picturePath}
-        >
-          <Transformation
-            height="70"
-            width="70"
-            gravity="face"
-            crop="fill"
-            radius="max"
-          />
-          <Placeholder type="pixelate" />
-        </Image>
-      </div>
-      <div className="col-7 text-left mt-3 ml-2">
-        <span>{userName}</span>
-        <br />
-        <span>{timeBefore}</span>
+    <div>
+      <ToastContainer
+        limit={1}
+        position="top-center"
+        autoClose={10000}
+        pauseOnHover
+        style={{
+          width: "50vw",
+        }}
+      />
+      <div
+        className="row ml-0 mt-2 "
+        style={{ overflowY: "visible", fontWeight: "500" }}
+        onClick={() => notify()}
+      >
+        <div className="col-3 text-right" style={{ paddingRight: "0" }}>
+          <Image
+            className="m-1"
+            cloudName={CloudName}
+            loading="lazy"
+            publicId={story.picturePath}
+          >
+            <Transformation
+              height="70"
+              width="70"
+              gravity="face"
+              crop="fill"
+              radius="max"
+            />
+            <Placeholder type="pixelate" />
+          </Image>
+        </div>
+        <div className="col-7 text-left mt-3 ml-2">
+          <span>{userName}</span>
+          <br />
+          <span>{timeBefore}</span>
+        </div>
       </div>
     </div>
   );

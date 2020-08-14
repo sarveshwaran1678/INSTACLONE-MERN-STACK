71f538a4e91206bf91758b8282bdf53da9e63bfe
Mayank Jain <jainmayank16041999@gmail.com>
2020-08-14 12:18:16 +0530

    request pending modal

--- projfrontend/src/PrivateScreens/UserProfileComponents/RequestsPendingModal.js
+++ projfrontend/src/PrivateScreens/UserProfileComponents/RequestsPendingModal.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import post from "../../Images/mayank.jpg";
 import { isAuthenticated } from "../../AuthScreens/APICalls/signCalls";
 import { toggleFollowRequest } from "./APICalls";
-import { getOwnUser } from "../UserFeedComponents/APICalls";
+import {
+  getOwnUser,
+  getAnotherUserDetails,
+} from "../UserFeedComponents/APICalls";
 
 const CloudName = process.env.REACT_APP_CLOUDNAME;
 
@@ -11,30 +14,50 @@ function RequestsPendingModal() {
   const token = isAuthenticated().token;
   //let pendingRequest = isAuthenticated().user.followRequestPending;
 
-  const [pendingRequest, setPendingRequest] = useState([]);
-  const [userDetails, setUserDetails] = useState({})
+  const [showData, setShowData] = useState(false);
+  const [userDetails, setUserDetails] = useState([]);
 
-  const [status, setStatus] = useState("");
+  //const [status, setStatus] = useState('');
 
-  useEffect(async() => {
-    await getUserDetails();
-    await userDetails()
+  useEffect(() => {
+    getUserDetails();
   }, []);
 
   const getUserDetails = async () => {
     await getOwnUser(userId, token)
       .then((res) => {
         const data = res.data;
+        console.log("Data", data.followRequestPending);
+        //setPendingRequest(data.followRequestPending);
 
-        setPendingRequest(data.followRequestPending);
+        data.followRequestPending.map(async (anotherUserId) => {
+          console.log("userDetails before", userDetails);
+
+          await getAnotherUserDetails(anotherUserId, userId, token)
+            .then((res) => {
+              const data = res.data;
+              userDetails.push({
+                id: data._id,
+                username: data.username,
+                profilePicPath: data.profilePicPath,
+              });
+              setUserDetails(userDetails);
+            })
+            .catch((err) => {
+              console.log("Not able to other user details for pending request");
+              console.log("ERR:", { ...err }.response);
+            });
+          console.log("userDetails after", userDetails);
+        });
       })
       .catch((err) => {
         console.log("Not able to get followPendingRequest ");
         console.log("ERR:", { ...err }.response);
       });
+    await setShowData(true);
   };
 
-  const updateRequest = async (anotherUserId) => {
+  const updateRequest = async (anotherUserId, status) => {
     await toggleFollowRequest(userId, anotherUserId, token, status)
       .then((res) => {})
       .catch((err) => {
@@ -42,23 +65,6 @@ function RequestsPendingModal() {
         console.log("ERR:", { ...err }.response.data);
       });
   };
-
-   const getAnotherUser = async (anotherUserId) => {
-    await getAnotherUserDetails(anotherUserId, userId, token)
-      .then((res) => {
-        const data = res.data;
-
-        setUserDetails({
-          id: data._id,
-          username: data.username,
-          profilePicPath: data.profilePicPath,
-         
-        });
-      })
-      .catch((err) => {
-        console.log("Not able to other user details for pending request");
-        console.log("ERR:", { ...err }.response);
-      });
 
   return (
     <div classname="container">
@@ -76,38 +82,57 @@ function RequestsPendingModal() {
       </div>
       <hr />
       <div style={{ maxHeight: "300px", overflowY: "scroll" }}>
-        <div className="d-flex flex-row bd-highlight justify-content-between align-items-center m-3">
-          <div>
-            {pendingRequest.map((request)=>)}
-            <img
-              src={post}
-              height={50}
-              width={50}
-              style={{ borderRadius: "50%", cursor: "pointer" }}
-              data-dismiss="modal"
-            />
-            <span
-              className="ml-2 "
-              style={{ fontWeight: "500", cursor: "pointer" }}
-              data-dismiss="modal"
-            >
-              Username
-            </span>
-          </div>
-          <div>
-            <i
-              class="fas fa-check fa-lg text-success mr-3"
-              data-dismiss="modal"
-              style={{ cursor: "pointer" }}
-            />
+        <div className="d-flex flex-row bd-highlight justify-content-between align-items-center mx-3 my-1">
+          {console.log(userDetails)}
+          {showData ? (
+            <React.Fragment>
+              {userDetails.map((user) => (
+                <React.Fragment>
+                  <div>
+                    <img
+                      src={post}
+                      height={50}
+                      width={50}
+                      style={{
+                        borderRadius: "50%",
+                        cursor: "pointer",
+                      }}
+                      data-dismiss="modal"
+                    />
+                    <span
+                      className="ml-2 "
+                      style={{ fontWeight: "500", cursor: "pointer" }}
+                      data-dismiss="modal"
+                    >
+                      {user.username}
+                    </span>
+                  </div>
+                  <div>
+                    <i
+                      class="fas fa-check fa-lg text-success mr-3"
+                      data-dismiss="modal"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => {
+                        console.log("tikked");
+                      }}
+                    />
 
-            <i
-              class="fas fa-times fa-lg text-danger "
-              data-dismiss="modal"
-              style={{ cursor: "pointer" }}
-            />
-          </div>
-        </div>{" "}
+                    <i
+                      class="fas fa-times fa-lg text-danger "
+                      data-dismiss="modal"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => {
+                        console.log("crossed");
+                      }}
+                    />
+                  </div>
+                </React.Fragment>
+              ))}
+            </React.Fragment>
+          ) : (
+            <div>No pending Request</div>
+          )}
+        </div>
       </div>
     </div>
   );
